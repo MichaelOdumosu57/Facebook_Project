ea36dc5c7c8c14969be6d70f8056ed642bd9c53a
Michael Odumosu <21322737+MichaelOdumosu57@users.noreply.github.com>
2023-05-03 19:24:52 -0400

    [UPDATE] additional work

--- ignore/set_backend_env.ps1
+++
@@ -1,5 +0,0 @@
-cd Env:
-Set-Content -Path IBM_APIKEY -Value 'xyrdWhc5pr6n-P7cHBoWw0CdZhyPN2R00ica7qVnp1XK'
-Set-Content -Path IBM_ENDPOINT -Value 'https://api.us-south.language-translator.watson.cloud.ibm.com/instances/ff50cf00-8514-4ad2-91b4-54368e6ccd6d'
-set-location C:\Users\Restop-2345\My_Notebook\angular\v11\Facebook\AngularApp\backend\ibm_language
-python tornado_server.py
